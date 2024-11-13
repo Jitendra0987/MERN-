1061246f2c99f1547f96b2a26e8e75735ac1b74b
Jitendra <jitendra01432@gmail.com>
2024-11-13 15:15:30 +0530

    express use postman

--- index.js
+++ index.js
@@ -3,11 +3,19 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res)=>{
- res.send("program of express framework")
 
+app.get("/",(req,res)=>{
+   res.send("this is get Response to the user!!!!!");
+})
+
+app.get("/save",(req,res)=>{
+   res.send("get requset by user send by server")
+})
+
+app.post("/save",(req,res)=>{
+   res.send("this os post res by serv er to user")
 })
 
 app.listen(8000,()=>{
-   console.log("server run on 8000!!!!")
-});
+   console.log("server run on 8000 port")
+})
