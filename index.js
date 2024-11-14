182e4b9e7ab9280f1d4154c06faa092c011c95c7
Jitendra <jitendra01432@gmail.com>
2024-11-15 00:44:41 +0530

    route create in express and hite on server

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-
 const express = require("express")
-const app =  express();
+const app = express();
 
 const stuRoute = require("./routes/studentRoute");
+
 app.use("/students",stuRoute);
 
 app.listen(8000,()=>{
-  console.log("server run on 8000")
+     console.log("server run on 8000!")
 })
