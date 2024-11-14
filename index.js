36aee7cedfb36da85a11c730938c0ec1e3ec580c
Jitendra <jitendra01432@gmail.com>
2024-11-14 15:37:27 +0530

    routes managment

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 
-const express = require("express");
- const app = express();
 
- app.get("/",(req,res)=>{
-   res.send("this is home page ")
- })
+const express = require("express")
+const app =  express();
 
- app.get("/about",(req,res)=>{
-   res.send("about page data")
- })
+const stuRoute = require("./routes/studentRoute");
+app.use("/students",stuRoute);
 
- app.post("/about",(req,res)=>{
-   res.send("About post Request data!!!")
- })
-
- app.get("/services",(req,res)=>{
-    res.send("our company server!!!!")
- })
-
- app.get("/images/photos/gellery",(req,res)=>{
-      const Data={
-         "rollno":121,
-         "name":"sachin",
-         "city":"Bhopal"
-      }
-      res.send(Data);
- })
-
- app.listen(8000,()=>{
-   console.log("server run on 8000")
- });
+app.listen(8000,()=>{
+  console.log("server run on 8000")
+})
